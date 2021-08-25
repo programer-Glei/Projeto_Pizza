c86f3af076db957f0669f2cf412f723c0e7bf6e5
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-08-25 19:00:15 -0300

    proximo comit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 pizzaJson.map((item, index)=>{
     let pizzaitem = document.querySelector('.models .pizza-item').cloneNode(true);
-    
+
+    // preencher as informações em pizzaitem
+
+    document.querySelector('.pizza-area').append(pizzaitem);
 })
