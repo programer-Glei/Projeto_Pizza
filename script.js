235227fcefb5592e199ff8554a8e9214472afa37
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-08-27 15:34:20 -0300

    proximo comit

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ pizzaJson.map((item, index)=>{
     let pizzaitem = e('.models .pizza-item').cloneNode(true);
     // preencher as informações em pizzaitem
     pizzaitem.querySelector('.pizza-item--img img').src = item.img
-    pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price}`
+    pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaitem.querySelector('a').addEventListener('click', (e) =>{
+        e.preventDefault()
+    })
     console.log(pizzaitem);
     console.log(item, index);
 
